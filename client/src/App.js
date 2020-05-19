c4d8523d62e0f247684370808b94a69ca4532a34
Blayze <blayzestone@protonmail.com>
2020-05-19 13:41:14 -0700

    create PrivateRoute component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
+import PrivateRoute from "./components/PrivateRoute";
+
+function Foobar() {
+  return <div>Protected</div>;
+}
 
 function App() {
   return (
     <div className="App">
       <Switch>
+        <PrivateRoute path="/protected" component={Foobar} />
         <Route path="/login" component={LoginForm} />
         <Route component={LoginForm} />
       </Switch>
